0c73d359523bef5c5c1f3683b2c71a5386444c6d
jecombe <jeremcombe@hotmail.fr>
2024-09-18 14:38:39 +0200

    uapte

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,14 +5,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployed = await deploy("EncryptedERC20", {
+  const deployed = await deploy("PrivatePool", {
     from: deployer,
-    args: ["testing", "TEST"],
+    args: ["0xeF3CbC4670C5ACdC672C0C5b8563668F5Ed3DeF3"],
     log: true,
   });
 
-  console.log(`EncryptedERC20 contract: `, deployed.address);
+  console.log(`PrivatePool contract: `, deployed.address);
 };
 export default func;
 func.id = "deploy_encryptedERC20"; // id required to prevent reexecution
-func.tags = ["EncryptedERC20"];
+func.tags = ["PrivatePool"];
